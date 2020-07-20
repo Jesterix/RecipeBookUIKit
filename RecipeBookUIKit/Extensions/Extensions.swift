ed5e6882a11a82c96802532d133ebf71482ec32a
Georgy Khaydenko <ghaidenko@gmail.com>
2020-07-20 20:35:38 +0300

    initial commit

---
+++ RecipeBookUIKit/Extensions/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  RecipeBookUIKit
+//
+//  Created by Георгий Хайденко on 20.07.2020.
+//  Copyright © 2020 George Khaydenko. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(text: String = "") {
+        self.init(frame: .zero)
+        self.attributedText = .init(string: text)
+    }
+}
--- RecipeBookUIKit/MainViewController.swift
+++ RecipeBookUIKit/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  RecipeBookUIKit
 //
 //  Created by Георгий Хайденко on 20.07.2020.
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
 
 
