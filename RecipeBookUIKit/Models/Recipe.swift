347fb434d6170663732c3b3e6a1d52ba3e34aad6
Georgy Khaydenko <ghaidenko@gmail.com>
2020-07-20 22:09:36 +0300

    add recipe model

---
+++ RecipeBookUIKit/Models/Recipe.swift
@@ -0,0 +1,13 @@
+//
+//  Recipe.swift
+//  RecipeBookUIKit
+//
+//  Created by Георгий Хайденко on 20.07.2020.
+//  Copyright © 2020 George Khaydenko. All rights reserved.
+//
+
+struct Recipe {
+    var title: String
+    var ingredients: [Ingredient] = []
+    var text: String = ""
+}
