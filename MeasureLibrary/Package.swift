9bed0d40c1e33d266512b1ba54c4f4d862c61110
Jesterix <ghaidenko@gmail.com>
2020-08-22 00:51:08 +0300

    add library package

---
+++ MeasureLibrary/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MeasureLibrary",
+    products: [
+        .library(
+            name: "MeasureLibrary",
+            targets: ["MeasureLibrary"]),
+    ],
+    dependencies: [
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "MeasureLibrary",
+            dependencies: []),
+        .testTarget(
+            name: "MeasureLibraryTests",
+            dependencies: ["MeasureLibrary"]),
+    ]
+)
---
+++ MeasureLibrary/README.md
@@ -0,0 +1,9 @@
+# MeasureLibrary
+
+A description of this package.
+
+This package helps to handle measurement initiating and converting from user input.
+
+Measure struct is basic struct to store all info for converting.
+Converter provides base operations.
+CustomMeasureProvider protocol helps to implement storing of user created Measures. 
